568a4e5580ec512896c596eaefa5cab7b9ac6f30
Akezhan Rakishev <akezhan.rakishev@tvh.com>
2022-12-23 15:26:42 +0600

    second commit

--- components/DAOCard.tsx
+++ components/DAOCard.tsx
@@ -3,23 +3,30 @@ import Link from 'next/link';
 
 type Props = {
   title: string;
-  price: string;
+  description: string;
   image: string;
+  tag: string;
+  status: string;
 }
 
-export const DAOCard: FC<Props> = ({ title, price, image }) => {
+export const DAOCard: FC<Props> = ({ title, description, image, tag, status }) => {
   return (
-    <div style={{ width: 250, backgroundColor: '#202a30', cursor: 'pointer' }} className="tw-relative tw-rounded-2xl tw-bg-card-blue tw-h-max tw-text-soft-white tw-text-lg tw-font-normal tw-mb-16 md:tw-mb-0 tw-shadow-xl">
+    <div style={{ width: 300, backgroundColor: '#202a30', cursor: 'pointer' }} className="tw-relative tw-rounded-2xl tw-bg-card-blue tw-h-max tw-text-soft-white tw-text-lg tw-font-normal tw-mb-16 md:tw-mb-0 tw-shadow-xl">
+      <div className="tw-absolute tw-top-2 tw-right-2 tw-px-2 tw-py-1 tw-rounded-lg" style={{ backgroundColor: '#232627' }}>
+        <p className="tw-text-sm tw-text-white tw-m-0">{tag}</p>
+      </div>
+      
       <Link href='/project/1' className="tw-block tw-relative tw-rounded-xl tw-overflow-hidden" title="Equilibrium">
-          <img src={image} className="tw-rounded-xl" style={{ width: 250, height: 175 }} alt="Equilibrium" />
+          <img src={image} className="tw-rounded-xl" style={{ width: 300, height: 210 }} alt="Equilibrium" />
       </Link>
       
-      <div className="px-3 py-2 tw-absolute tw-bottom-0 tw-bg-black tw-rounded-b-xl" style={{ width: '100%', opacity: '0.87' }}>
-        <div className="tw-text-xl tw-font-semibold tw-mb-2">
-          <a style={{ textDecoration: 'none' }} className="tw-text-white hover:tw-text-white tw-transition-colors tw-duration-1000">{title}</a>
+      <div className="px-3 py-2 tw-absolute tw-bottom-0 tw-rounded-b-xl" style={{ width: '100%', height: 90, backgroundColor: 'rgba(0,0,0, 0.85)' }}>
+        <div className="tw-flex tw-justify-between tw-items-center tw-mb-2">
+          <p className="tw-text-lg tw-font-semibold tw-text-white hover:tw-text-white tw-transition-colors tw-duration-1000 tw-m-0">{title}</p>
+          <p style={{ zIndex: 100, color: '#9fef00', fontSize: 14 }} className="tw-font-semibold hover:tw-text-white tw-transition-colors tw-duration-1000 tw-m-0">{status}</p>
         </div>
         
-        <p className="tw-text-sm text-white tw-font-semibold tw-tracking-wide tw-ml-1 tw-m-auto">{price}</p>
+        <p className="tw-text-sm text-white tw-tracking-wide tw-m-0">{description}</p>
       </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,45 @@ import { DAOCard } from "../components/DAOCard";
 const projects = [
   {
     title: "Bitgaming.me",
-    capital: "268 mln $",
-    image: "bitgaming.png",
+    description: "Play-To-Earn game",
+    image: "app1.png",
+    tag: "Games",
+    status: "Launched",
   },
   {
     title: "Uniswap",
-    capital: "659 mln $",
-    image: "uniswap.jpeg",
+    description: "Crypto exchange platform",
+    image: "app2.jpeg",
+    tag: "Exchange",
+    status: "Launched",
   },
   {
-    title: "Uniswap",
-    capital: "659 mln $",
-    image: "uniswap.jpeg",
+    title: "Mummy",
+    description: "The Next Gen Ancient World.",
+    image: "app3.png",
+    tag: "Games",
+    status: "Coming soon",
+  },
+  {
+    title: "Ultiverse",
+    description: "Connecting Web3 through a AAA Gaming metaverse",
+    image: "app4.png",
+    tag: "Games",
+    status: "Coming soon",
+  },
+  {
+    title: "EX Sports",
+    description: "EX Sport Starz - platform that allows sport fans to trade digital assets.",
+    image: "app5.png",
+    tag: "Sport",
+    status: "Launched",
+  },
+  {
+    title: "Neo Fantasy",
+    description: "Play With Courage & Benefit!",
+    image: "app6.png",
+    tag: "Games",
+    status: "Coming soon",
   },
 ]
 
@@ -28,8 +55,8 @@ const App: FC = () => {
       <div className="row">
         {
           projects.map((n, i) => (
-            <div key={i} className="col-3">
-              <DAOCard title={n.title} price={n.capital} image={n.image} />
+            <div key={i} className="col-3 mb-4">
+              <DAOCard {...n} />
             </div>
           ))
         }
